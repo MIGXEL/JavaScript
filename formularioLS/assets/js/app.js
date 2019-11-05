e61401e4896613b82eb2058248faf196913230fb
MIGUEL <miguelantiman.mav@gmail.com>
2019-11-05 12:56:57 -0300

    faltaba guardar un lineas de codigos

--- formularioLS/assets/js/app.js
+++ formularioLS/assets/js/app.js
@@ -20,14 +20,19 @@ function agregarTweet(e) {
     e.preventDefault();
     // Leer valor del TextArea
     const tweet = document.getElementById('tweet').value;
+
     // Crear boton de eliminar
     const botonBorrar = document.createElement('a');
     botonBorrar.classList = 'borrar-tweet';
     botonBorrar.innerText = 'X';
+
     // Crear elemento y agregarle el contenido al elemento
     const li = document.createElement('li');
     li.innerText = tweet;
+    //Añade el boton de borrar Tweet
     li.appendChild(botonBorrar);
+    //Añade el tweet a la lista
     listaTweets.appendChild(li);
+
     console.log(tweet);
 }
